ca9e6fbf91fa173beaf10b3185c239de8be1077b
Adam Bramley <adam.bramley@justice.vic.gov.au>
2016-11-18 13:58:09 +1100

    Test passing

--- tests/src/Kernel/EntityReferenceRevisionItemNormalizerTest.php
+++ tests/src/Kernel/EntityReferenceRevisionItemNormalizerTest.php
@@ -63,8 +63,6 @@ class EntityReferenceRevisionItemNormalizerTest extends KernelTestBase {
       'id' => $paragraph_id,
     ]);
     $paragraph_type->save();
-
-    // Add a paragraph field to the article.
     $node_para_field = 'node_paragraph_field';
     $field_storage = FieldStorageConfig::create([
       'field_name' => $node_para_field,
@@ -82,20 +80,25 @@ class EntityReferenceRevisionItemNormalizerTest extends KernelTestBase {
     ]);
     $field->save();
 
+    // Create paragraphs, cloning before saving so that when the unsaved uuid
+    // resolver returns the clones, they are saved along with the node.
     $paragraph1 = Paragraph::create([
       'title' => 'Paragraph',
       'type' => $paragraph_id,
     ]);
+    $clone_p_1 = clone $paragraph1;
     $paragraph1->save();
     $paragraph2 = Paragraph::create([
       'title' => 'Paragraph',
       'type' => $paragraph_id,
     ]);
+    $clone_p_2 = clone $paragraph2;
     $paragraph2->save();
     $paragraph3 = Paragraph::create([
       'title' => 'Paragraph',
       'type' => $paragraph_id,
     ]);
+    $clone_p_3 = clone $paragraph3;
     $paragraph3->save();
 
     // Create a node with two paragraphs.
@@ -108,16 +111,19 @@ class EntityReferenceRevisionItemNormalizerTest extends KernelTestBase {
 
     // Normalize the node and test we can denormalize.
     $serializer = $this->container->get('serializer');
-    $context['included_fields'] = ['uuid'];
+    $link_manager = $this->container->get('rest.link_manager');
+    $field_uri = $link_manager->getRelationUri('node', 'article', $node_para_field, []);
 
-    $normalized = $serializer->normalize($node, 'hal_json', $context);
-    // Now we switch the URI to something else but it should still go back to
-    // the same node.
-    $normalized['link'][0]['link'] = 'entity:node/' . ($node->id() + 1);
+    $normalized = $serializer->normalize($node, 'hal_json');
+    // Change the revision ids to something else to prove it is the uuid
+    // normalization we are testing.
+    $normalized['_embedded'][$field_uri][0]['target_revision_id'] = $paragraph1->getRevisionId() + 4;
+    $normalized['_embedded'][$field_uri][1]['target_revision_id'] = $paragraph2->getRevisionId() + 5;
+    $normalized['_embedded'][$field_uri][2]['target_revision_id'] = $paragraph3->getRevisionId() + 6;
+
     $unsaved_uuid_resolver = $this->container->get('entity_pilot.resolver.unsaved_uuid');
-    $clone_p_1 = clone $paragraph1;
-    $clone_p_2 = clone $paragraph1;
-    $clone_p_3 = clone $paragraph3;
+
+    // Add our unsaved entities manually.
     $unsaved_uuid_resolver->add($clone_p_1);
     $unsaved_uuid_resolver->add($clone_p_2);
     $unsaved_uuid_resolver->add($clone_p_3);
@@ -132,10 +138,13 @@ class EntityReferenceRevisionItemNormalizerTest extends KernelTestBase {
     $paragraph3->delete();
 
     /** @var \Drupal\node\NodeInterface $denormalized */
-    $denormalized = $serializer->denormalize($normalized, NodeInterface::class, 'hal_json');
+    $denormalized = $serializer->denormalize($normalized, Node::class, 'hal_json');
+    $denormalized->save();
     $entities = $denormalized->{$node_para_field}->referencedEntities();
+
     $this->assertEquals($paragraph1_uuid, $entities[0]->uuid());
     $this->assertEquals($paragraph2_uuid, $entities[1]->uuid());
     $this->assertEquals($paragraph3_uuid, $entities[2]->uuid());
   }
+
 }
